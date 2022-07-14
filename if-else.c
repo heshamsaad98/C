b0ec4705753096b2862f14e332ee640bd7c8c1de
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-07-14 16:34:39 -0700

    Create if-else.c

---
+++ if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10;
+    int y = 5;
+    
+    if(x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else
+    {
+        printf("x is not less than y\n");
+    }
+}
