d264f1db43f0bdc9f138e2d803ea4625d0788149
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-07-14 16:40:42 -0700

    Create else-if.c

---
+++ else-if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5;
+    int y = 5;
+    
+    if(x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if(x > y)
+    {
+        printf("x is not less than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
+}
