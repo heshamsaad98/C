56e6584c9ac24c35fc807dfbef0da3a5c846cebd
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-02 16:10:14 -0700

    Update string6.c

--- string6.c
+++ string6.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    string s = "HI!";
+
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
+}
