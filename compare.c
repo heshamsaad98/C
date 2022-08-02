f0da5a6b196d719a7082acc3a7944e4cbb684320
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-02 16:32:45 -0700

    Create compare.c

---
+++ compare.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string>
+
+int main(void)
+{
+    string s = get_string("s: ");
+    string t = get_string("t: ");
+
+    if(strcmp(s, t) == 0)
+    {
+        printf("Same\n");
+    }
+    else
+    {
+        printf("Different\n");
+    }
+}
