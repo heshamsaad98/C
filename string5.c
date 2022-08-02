596cd3dcb0987a6028c2313e4680cfc068616788
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-02 16:08:23 -0700

    Create string5.c

---
+++ string5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string s = "HI!";
+
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
+    printf("%c\n", s[3]);
+}
