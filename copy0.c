80e6a1a3f2f84eb470c1ca9c10ae27e6af1a1c33
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-02 16:48:00 -0700

    Create copy0.c

---
+++ copy0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = malloc(strlen(s) + 1);
+
+    for (int i = 0; i < strlen(s) + 1; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
