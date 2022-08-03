a6f62da0f0b151186ff022d883207914b87b610d
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-02 19:06:29 -0700

    Create array0.c

---
+++ array0.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *list = malloc(3*sizeof(int));
+
+    if (list == NULL)
+    {
+        return 1;
+    }
+
+    *list = 1; // list[0]
+    *(list + 1) = 2; // list[1]
+    *(list + 2) = 3; // list[2]
+
+    int *tmp = malloc(4*sizeof(int));
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    *(tmp + 3) = 4;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", tmp[i]);
+    }
+}
